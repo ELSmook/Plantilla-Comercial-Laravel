f18cc878aae59fe43688fa52024d5a247f93a092
ELSmook <130613758+ELSmook@users.noreply.github.com>
2026-04-17 13:44:30 -0700

    imgQRresize

--- resources/views/livewire/dynamic-photos.blade.php
+++ resources/views/livewire/dynamic-photos.blade.php
@@ -3,7 +3,7 @@
    <div wire:poll.20s>
     @foreach($fotos as $foto)
         <div class="carousel-item @if($loop->first) active @endif">
-            <img src="{{ Storage::url($foto->imagen) }}" class="img-xl" alt="Foto ampliada">
+            <img src="{{ Storage::url($foto->imagen) }}" class="img-bg" alt="Foto ampliada">
         </div>
     @endforeach
 </div>

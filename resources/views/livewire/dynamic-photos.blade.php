b2cb658a897c22e9a7b1ff78e482eaeb94b167af
ELSmook <130613758+ELSmook@users.noreply.github.com>
2026-04-17 13:48:48 -0700

    imgQRresize2

--- resources/views/livewire/dynamic-photos.blade.php
+++ resources/views/livewire/dynamic-photos.blade.php
@@ -1,9 +1,9 @@
 <div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+    {{-- Because she competes with no one, no one can compete with her. class="img-bg" --}}
    <div wire:poll.20s>
     @foreach($fotos as $foto)
         <div class="carousel-item @if($loop->first) active @endif">
-            <img src="{{ Storage::url($foto->imagen) }}" class="img-bg" alt="Foto ampliada">
+            <img src="{{ Storage::url($foto->imagen) }}" width="500px" height="500px" alt="Foto ampliada">
         </div>
     @endforeach
 </div>
